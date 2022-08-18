1c6f41288c510549c298fecd240f14744792b7c5
PedroLopes7K <pedrohenri3601@gmail.com>
2022-08-17 23:30:28 -0300

    Html finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'facebook-blue': '#1877f2'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
